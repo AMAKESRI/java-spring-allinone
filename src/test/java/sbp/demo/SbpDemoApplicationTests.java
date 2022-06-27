d85642ec24009e94b60dd2e43f30527b202ab5ac
massaoud <mouhssine.assaoud@nimbleways.com>
2022-06-27 16:06:42 +0100

    Init an empty spring boot project initialized with spring web and spring security + fix the port at 8081

---
+++ src/test/java/sbp/demo/SbpDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package sbp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbpDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
